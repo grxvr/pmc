148847548305fb56f10fa66b224e7119134e4b09
grxvr <bagnukbogdan5@gmail.com>
2026-08-29 16:27:39 +0300

    :)

--- expressions.py
+++ expressions.py
@@ -1,11 +1,11 @@
 from math import pi, sqrt
 
 a = 100 / pi #test @nobody
-b = ( a / pi) + ( 228 - 1337) # sex
-
+b = (a / pi) + (228 - 1337) # sex
 c = sqrt(a**2 + b**2) # supersex
+
 #\n
+
 a = a * pi
 b = 3*a - sqrt(a**3)
 c = sqrt(a**24+ b**2)
-
